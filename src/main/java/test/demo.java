00eda69fe7792d05fe1d8b204f31bf588d7160db
zhenghaobin <591714809@qq.com>
2022-09-24 21:22:13 +0800

    demo3

--- src/main/java/test/demo.java
+++ src/main/java/test/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("666");
         System.out.println("888" );
+        System.out.println("999" );
     }
 }
