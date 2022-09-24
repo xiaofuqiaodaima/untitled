14e284e4033e96310d05a6d4285062105fb033c3
zhenghaobin <591714809@qq.com>
2022-09-24 22:40:07 +0800

    master_demo2

--- src/main/java/test/demo.java
+++ src/main/java/test/demo.java
@@ -6,5 +6,6 @@ public class demo {
         System.out.println("888" );
         System.out.println("999" );
         System.out.println("555" );
+        System.out.println("master test!" );
     }
 }
